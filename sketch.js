e00540968111fdedca11415cb55c8f76e5d1d622
Mubashir42884 <mubashir.mohsin.42884@gmail.com>
2024-09-01 15:27:11 +0600

    Week-01

---
+++ sketch.js
@@ -0,0 +1,30 @@
+let mobilenet;
+let messi;
+
+function modelReady() {
+    console.log('Model Is READY');
+    // Use the classify method instead of predict
+    mobilenet.classify(messi, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error){
+        console.error(error);
+    } else{
+        console.log(results);
+    }
+}
+
+function imageReady() {
+    image(messi, 0, 0, width, height);
+}
+
+function setup() {
+    createCanvas(640, 640);
+    messi = createImg('Images/messi.jpg', imageReady);
+    messi.hide();
+    background(0);
+
+    // Load the MobileNet model using ml5's imageClassifier
+    mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+}
